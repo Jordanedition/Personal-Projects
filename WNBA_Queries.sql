e53fb22296f99bcd4797f3f06209e6f47ead4e67
Jordan Solomon <martinmssolomon@gmail.com>
2026-02-25 22:01:08 -0500

    Update WNBA_Queries.sql

--- WNBA_Queries.sql
+++ WNBA_Queries.sql
@@ -1,18 +1,18 @@
 #Sacramento Monarchs Historical Scouting Success
 SELECT 
-    player, 
-    year, 
-    overall_pick, 
-    win_shares
+    w.player, 
+    w.year, 
+    w.overall_pick, 
+    w.win_shares
 FROM wnbadraft w 
-WHERE team = 'Sacramento Monarchs'
-ORDER BY win_shares DESC;
+WHERE w.team = 'Sacramento Monarchs'
+ORDER BY w.win_shares DESC;
 
 #Draft Efficency Draft Asset Valuation
 SELECT 
-    overall_pick, 
-    AVG(win_shares * 1.0) AS avg_career_value,
-    AVG(points * 1.0) AS avg_points_per_game
+    w.overall_pick, 
+    AVG(w.win_shares * 1.0) AS avg_career_value,
+    AVG(w.points * 1.0) AS avg_points_per_game
 FROM wnbadraft w 
-GROUP BY overall_pick
-ORDER BY overall_pick ASC;
+GROUP BY w.overall_pick
+ORDER BY w.overall_pick ASC;
